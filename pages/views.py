5a8b6e36d78bffbb181d3cfe5177dfb4bc690a4a
Rober <boblawyer1@gmail.com>
2023-02-21 00:17:53 -0300

    creados los templates y los tests

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import HttpResponse
+from django.views.generic import TemplateView
 
-def homePageView(request):
-    return HttpResponse("Hello World")
+class HomePageView(TemplateView):
+    template_name = 'home.html' 
 
-# Create your views here.
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
